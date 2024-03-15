f6524c8e03ad85a7a642cd7e32f04c4367d8bdc0
Derick J Falkowski <derickjosuefalkowski@gmail.com>
2024-03-15 10:51:54 -0300

    Adicionando os projetos de Ambientação e da Lista de Exercicios

---
+++ Ambientacao/ListaExercicios/Exe06A/Program.cs
@@ -0,0 +1,36 @@
+﻿//Criar um vetor
+int[] vetor = new int[100];
+Random aleatorio = new Random();
+
+//Preencher o vetor aleatório
+for(int i = 0; i < vetor.Length; i++) {
+    vetor[i] = aleatorio.Next(100);
+}
+
+for(int i = 0; i < vetor.Length; i++) {
+    Console.Write(vetor[i] + " ");
+}
+
+//Ordernar o vetor
+bool troca;
+do
+{
+    troca = false;
+    for(int i = 0; i < vetor.Length - 1; i++) {
+
+        if(vetor[i] > vetor[i + 1]) {
+            int aux = vetor[i];
+            vetor[i] = vetor[i +1];
+            vetor[i +1] = aux;
+            troca = true;
+        }
+    }
+} while(troca == true);
+
+Console.WriteLine("\n");
+
+//Imprimir o vetor
+for(int i = 0; i < vetor.Length; i++) {
+    Console.Write(vetor[i] + " ");
+}
+
---
+++ Ambientacao/ListaExercicios/Exe06B/Program.cs
@@ -0,0 +1,23 @@
+﻿//Criar um vetor
+int[] vetor = new int[100];
+Random aleatorio = new Random();
+
+//Preencher o vetor aleatório
+for(int i = 0; i < vetor.Length; i++) {
+    vetor[i] = aleatorio.Next(100);
+}
+
+for(int i = 0; i < vetor.Length; i++) {
+    Console.Write(vetor[i] + " ");
+}
+
+//Ordernar o vetor
+Array.Sort(vetor);
+
+Console.WriteLine("\n");
+
+//Imprimir o vetor
+for(int i = 0; i < vetor.Length; i++) {
+    Console.Write(vetor[i] + " ");
+}
+
